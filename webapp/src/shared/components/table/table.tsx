f8866fe470fbacc498a8c247b8f922e9402dfe57
Jardel Sousa <jardeelsousa@hotmail.com>
2022-05-13 18:12:55 -0300

    Teste Branch

--- webapp/src/shared/components/table/table.tsx
+++ webapp/src/shared/components/table/table.tsx
@@ -9,6 +9,7 @@ import {
 import { Box, createTheme, Paper, Typography } from '@mui/material';
 import { GridDDados } from '../../types/dados.types';
 import { GridToolbarFilter } from '@mui/x-data-grid';
+//TESTE Branch
 
 interface ITableProps {
 	DDadosGrid: GridDDados;
